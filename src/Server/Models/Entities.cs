73b2f4d612d008707545ef9749859a00b3196499
steniojoseph27 <steniojoseph@yahoo.com>
2025-09-07 23:03:55 -0400

    Create Entities class with 2 classes: ModelSnapshot, ContextEvent.

---
+++ src/Server/Models/Entities.cs
@@ -0,0 +1,20 @@
+namespace Server.Models
+{
+    class ModelSnapshot
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string SessionId { get; set; } = "";
+        public DateTime CreateUtc { get; set; } = DateTime.UtcNow;
+        public string Source { get; set; } = "Revit";
+        public string Json { get; set; } = "";
+    }
+
+    class ContextEvent
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string SessionId { get; set; } = "";
+        public DateTime CreateUtc { get; set; } = DateTime.UtcNow;
+        public string Kind { get; set; } = "";
+        public string Payload { get; set; } = "";
+    }
+}
